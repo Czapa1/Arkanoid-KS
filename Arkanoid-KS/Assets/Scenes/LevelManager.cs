7773df84d4d262b6915b6edb3e7020e114de5944
karolhonkisz7 <kacperstanclik1@gmail.com>
2023-10-04 17:09:45 +0200

    Added LevelManager script

---
+++ Arkanoid-KS/Assets/Scenes/LevelManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+using Unity.VisualScripting;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene("Lose");
+    }
+
+    void Update()
+    {
+        
+    }
+}
